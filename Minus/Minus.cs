e4138f31745be4d0729e564a293694d2f8a3a16e
Xodake <xodake@gmail.com>
2017-02-13 16:53:21 +0200

    Add class Minus

---
+++ Minus/Minus.cs
@@ -0,0 +1,60 @@
+﻿using Scram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minus
+{
+    public abstract class Minus : IOperation, IOperationExeption
+    {
+       
+        public string discription
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        MyInput num;
+        MyInput num2;
+       
+        public double Result()
+        {
+
+            return num.get_number() - num2.get_number();
+        }
+    }
+}
---
+++ Minus/MyInput.cs
@@ -0,0 +1,35 @@
+﻿using Scram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minus
+{
+    class MyInput : Iinput
+    {
+        double number1;
+        
+        public string formatt
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public MyInput() {
+           number1=Convert.ToDouble( Console.ReadLine());
+        }
+        public double get_number()
+        {
+            return number1;   
+        }
+    }
+}
